0351ab8e973a3f30d582870289e6896f1fd7f7a4
Sebastian Schramm <s-schramm1@gmx.de>
2020-12-05 13:36:00 +0100

    create chapter_01

---
+++ chapter_01/src/chapter_01/Main.java
@@ -0,0 +1,18 @@
+package chapter_01;
+
+/**
+ * Klasse mit der Main-Methode
+ * @author sebastian
+ *
+ */
+public class Main {
+	
+	/**
+	 * Die Main Methode
+	 * Gibt "Dear Prudence" aus
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Dear Prudence");
+	}
+}
